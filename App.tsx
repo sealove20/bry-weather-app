4bc2fc9756e829fd96c330be12720392ce87ba77
Matheus Henrique <matheus.181137@gmail.com>
2024-10-26 08:09:23 -0300

    feat: create app main flow

--- App.tsx
+++ App.tsx
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeAreStyle}>
+      <View style={styles.weatherForecast}>
+        <Text style={styles.city}>Florianópolis</Text>
+        <Text style={styles.temperature}>22°C</Text>
+        <Text style={styles.humidity}>Umidade 83%</Text>
+      </View>
+      <View style={styles.weatherForecastMiniatureContainer}>
+        <View style={styles.weatherForecastMiniature}>
+          <Text style={styles.temperature}>22°C</Text>
+          <Text style={styles.humidity}>Umidade 83%</Text>
+        </View>
+        <View style={styles.weatherForecastMiniature}>
+          <Text style={styles.temperature}>22°C</Text>
+          <Text style={styles.humidity}>Umidade 83%</Text>
+        </View>
+      </View>
+      <StatusBar style="auto" translucent />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreStyle: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center"
   },
+  weatherForecast: {
+    alignItems: "center",
+    width: "80%",
+    height: "30%",
+    backgroundColor: "gray",
+    borderRadius: 5,
+    marginTop: 15
+  },
+  temperature: {
+    fontSize: 30,
+    color: "white"
+  },
+  city: {
+    fontSize: 24,
+    color: "white"
+  },
+  humidity: {
+    fontSize: 24, 
+    color: "white"
+  },
+  weatherForecastMiniature: {
+    alignItems: "center",
+    width: 80,
+    height: 200,
+    backgroundColor: "gray",
+    borderRadius: 5,
+    marginTop: 15
+  },
+  weatherForecastMiniatureContainer: {
+    flexDirection: "row",
+    gap: 15
+  }
 });
