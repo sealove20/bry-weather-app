68132cb2bc3610e6492d5433e4bdc696f8564d12
Matheus Henrique <matheus.181137@gmail.com>
2024-10-28 19:29:44 -0300

    refactor: change resource file extension

--- src/resources/weather/weather.ts
+++ src/resources/weather/weather.ts
@@ -1,4 +1,4 @@
-import { Forecast, UserCoordinates } from "./types";
+import { AutocompleteResponse, Forecast, UserCoordinates } from "./types";
 
 export const getByUserCoordinates = async (userCoordinates: UserCoordinates): Promise<Forecast> => {
   const { longitude, latitude } = userCoordinates;
@@ -12,6 +12,14 @@ export const getByUserCoordinates = async (userCoordinates: UserCoordinates): Pr
 export const getByCityName = async (city: string): Promise<Forecast> => {
   const response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=${process.env.EXPO_PUBLIC_API_KEY}&q=${city}&days=3&aqi=no&alerts=no`,
+  );
+  const json = await response.json();
+  return json;
+};
+
+export const getAutocomplete = async (searchedCity: string): Promise<AutocompleteResponse[]> => {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/search.json?key=${process.env.EXPO_PUBLIC_API_KEY}&q=${searchedCity}`,
   );
   const json = await response.json();
   return json;
