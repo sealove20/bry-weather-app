f8b0b46a7e80e1679e508e007fe3085ee1a1dc04
Matheus Henrique <matheus.181137@gmail.com>
2024-10-28 12:20:08 -0300

    test: CustomText component

---
+++ components/Text/CustomText.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react-native";
+import { fontSize as fontSizeTokens } from "../../tokens/fontSize";
+import CustomText from "./CustomText";
+
+describe("CustomText", () => {
+  it("renders correctly with the extra small size", () => {
+    render(<CustomText size="xsm">Extra Small Text</CustomText>);
+    const textElement = screen.getByText("Extra Small Text");
+
+    expect(textElement).toBeTruthy();
+    expect(textElement.props.style[1].fontSize).toBe(fontSizeTokens.xsm);
+  });
+
+  it("renders correctly with the small size", () => {
+    render(<CustomText size="sm">Small Text</CustomText>);
+    const textElement = screen.getByText("Small Text");
+
+    expect(textElement).toBeTruthy();
+    expect(textElement.props.style[1].fontSize).toBe(fontSizeTokens.sm);
+  });
+
+  it("renders correctly with the medium size", () => {
+    render(<CustomText size="md">Medium Text</CustomText>);
+    const textElement = screen.getByText("Medium Text");
+
+    expect(textElement).toBeTruthy();
+    expect(textElement.props.style[1].fontSize).toBe(fontSizeTokens.md);
+  });
+
+  it("renders correctly with the large size", () => {
+    render(<CustomText size="lg">Large Text</CustomText>);
+    const textElement = screen.getByText("Large Text");
+
+    expect(textElement).toBeTruthy();
+    expect(textElement.props.style[1].fontSize).toBe(fontSizeTokens.lg);
+  });
+
+  it("renders correctly with the extra large size", () => {
+    render(<CustomText size="xl">Extra Large Text</CustomText>);
+    const textElement = screen.getByText("Extra Large Text");
+
+    expect(textElement).toBeTruthy();
+    expect(textElement.props.style[1].fontSize).toBe(fontSizeTokens.xl);
+  });
+});
