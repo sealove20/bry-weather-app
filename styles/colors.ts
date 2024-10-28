2a4c5b927fe050d432b2ed637646f1bcc64b0e83
Matheus Henrique <matheus.181137@gmail.com>
2024-10-27 23:21:37 -0300

    feat: create color tokens

---
+++ styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  white: "white",
+  blue: {
+    light: "#BFECFF",
+    semiLight: "#00D4FF",
+    dark: "#29BFFF",
+  },
+  gray: {
+    transparent: "rgba(100, 100, 100, 0.6)",
+  },
+};
