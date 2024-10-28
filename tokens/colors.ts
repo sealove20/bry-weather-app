6dc3ac23dce9e528ca8f6cd862d99c08255ebda6
Matheus Henrique <matheus.181137@gmail.com>
2024-10-28 12:17:31 -0300

    feat: add new dark colors

--- tokens/colors.ts
+++ tokens/colors.ts
@@ -3,9 +3,15 @@ export const colors = {
   blue: {
     light: "#BFECFF",
     semiLight: "#00D4FF",
-    dark: "#29BFFF",
+    dark: "rgba(10, 25, 200, 0.5)",
   },
   gray: {
     transparent: "rgba(100, 100, 100, 0.6)",
+  },
+  yellow: {
+    dark: "rgba(255, 200, 0, 0.7)",
+  },
+  red: {
+    dark: "rgba(255, 0, 0, 0.6)",
   },
 };
