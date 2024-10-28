90e03d16bbd19548f1b7a1cc7f2ff978fb36576e
Matheus Henrique <matheus.181137@gmail.com>
2024-10-28 19:30:37 -0300

    feat: create debounce function

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,13 @@
+export const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+): ((...args: Parameters<T>) => void) => {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>): void => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
