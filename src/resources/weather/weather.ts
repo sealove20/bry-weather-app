473be54dc65b8b75132ff18e72f5dd22cc3f335b
Matheus Henrique <matheus.181137@gmail.com>
2024-10-29 16:42:58 -0300

    refactor: create constant values for baseURL and apiKey

--- src/resources/weather/weather.ts
+++ src/resources/weather/weather.ts
@@ -1,9 +1,16 @@
 import { AutocompleteResponse, Forecast, UserCoordinates } from "./types";
 
+const BASE_URL = "https://api.weatherapi.com/v1";
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+
+if (!API_KEY) {
+  throw new Error("API key is missing. Please set EXPO_PUBLIC_API_KEY.");
+}
+
 export const getByUserCoordinates = async (userCoordinates: UserCoordinates): Promise<Forecast> => {
   const { longitude, latitude } = userCoordinates;
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=${process.env.EXPO_PUBLIC_API_KEY}&q=${latitude},${longitude}&days=3&aqi=no&alerts=no`,
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=3&aqi=no&alerts=no`,
   );
   const json = await response.json();
   return json;
@@ -11,16 +18,14 @@ export const getByUserCoordinates = async (userCoordinates: UserCoordinates): Pr
 
 export const getByCityName = async (city: string): Promise<Forecast> => {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=${process.env.EXPO_PUBLIC_API_KEY}&q=${city}&days=3&aqi=no&alerts=no`,
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`,
   );
   const json = await response.json();
   return json;
 };
 
 export const getAutocomplete = async (searchedCity: string): Promise<AutocompleteResponse[]> => {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/search.json?key=${process.env.EXPO_PUBLIC_API_KEY}&q=${searchedCity}`,
-  );
+  const response = await fetch(`${BASE_URL}/search.json?key=${API_KEY}&q=${searchedCity}`);
   const json = await response.json();
   return json;
 };
